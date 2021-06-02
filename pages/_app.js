8caed9c12ec34753217f0437ea802326b1f44d75
Geth <mashven@gmail.com>
2021-06-02 10:20:43 +0530

    Created document.js file to include fontawesome icon cdn
    added cdn file to document.js for proper rendering of icons

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,6 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Website to handle DMCA takedowns" />
-        {/*add script to add fontawesome icon library */}
-        <script
-          src="https://kit.fontawesome.com/900c52cd77.js"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
 
       <Layout>
